4df4353e49792b696e09d8cd5a85e58f2e081ab1
Prasad528260 <prasadsubhedar5460@gmail.com>
2026-01-17 10:28:04 +0530

    payment test

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,6 +1,10 @@
 import instance from "../utils/razorpay.js";
 import Payment from "../models/Payment.js";
 import { amount } from "../utils/constants.js";
+import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils.js";
+import User from "../models/User.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const createPayment = async (req, res) => {
   try {
@@ -38,3 +42,41 @@ export const createPayment = async (req, res) => {
   }
 };
 
+export const verifyPayment = async (req, res) => {
+  try {
+    const webhookSignature = req.get("x-razorpay-signature");
+    const isWebhookValid = validateWebhookSignature(JSON.stringify(req.body),webhookSignature,process.env.RAZORPAY_WEBHOOK_SECRET);
+
+    if(!isWebhookValid){
+      return res.status(400).json({
+        message: "Invalid webhook signature",
+      });
+    }
+
+    // UPDATE PAYMENT STATUS
+    const paymentdetails = req.body.payload.payment.entity;
+    const payment = await Payment.findOne({ orderId: paymentdetails.order_id });
+    payment.status = paymentdetails.status;
+    await payment.save();
+    // MAKE USER PREMIUM
+    const user = await User.findById(payment.userId);
+    user.plan = "premium";
+    await user.save();
+
+
+    if(req.body.event === "payment.captured"){
+      // Update payment status
+    }
+    if(req.body.event === "payment.failed"){
+      // Update payment status
+    }
+
+    return res.status(200).json({
+      message: "Payment verification successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Payment verification failed" + error.message,
+    });
+  }
+};
--- src/routes/paymentRouter.js
+++ src/routes/paymentRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { userAuth } from "../middleware/userAuth.js";
-import { createPayment } from "../controllers/paymentController.js";
+import { createPayment, verifyPayment } from "../controllers/paymentController.js";
 const paymentRouter = express.Router();
 
 paymentRouter.post("/create", userAuth, createPayment)
+paymentRouter.post("/webhook", verifyPayment)
 
 export default paymentRouter;
